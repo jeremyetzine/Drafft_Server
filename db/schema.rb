f885e0e32bed7278350fafd3f05b839e2fa8524d
JETZ <jeremyetzine@hotmail.com>
2019-03-04 10:32:09 +1100

    first seeds added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_053912) do
+ActiveRecord::Schema.define(version: 2019_03_03_233105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 2019_03_03_053912) do
     t.integer "squad_id"
     t.string "name"
     t.string "abbr"
-    t.string "thumbnail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
