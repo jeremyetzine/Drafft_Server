3d86379fde024021a6019b23a22f4dce3f7a2570
JETZ <jeremyetzine@hotmail.com>
2019-03-07 15:13:18 +1100

    set up squads auto create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   after_create :add_user_to_draft
+  after_create :add_squad_to_user
 
   has_many :squads
   belongs_to :draft, optional: true
@@ -9,5 +10,13 @@ class User < ApplicationRecord
 
   def add_user_to_draft
     Draft.all.first.users << self
+  end
+
+  def add_squad_to_user
+    s = Squad.new
+    s.name = self.username + 'squad'
+    s.draft_id = 1
+    self.squads << s
+    s.save
   end
 end
